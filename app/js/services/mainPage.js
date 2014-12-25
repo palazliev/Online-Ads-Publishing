f589a65644c09f3b8df8a18ff5259b8d788094a4
palazliev <palazliev@gmail.com>
2014-12-25 16:01:20 +0200

    fixed image width and height

--- app/js/services/mainPage.js
+++ app/js/services/mainPage.js
@@ -8,6 +8,15 @@ softUni.factory('mainData', function($http, $log){
                 .error(function(data, status, headers, config){
                     $log.warn(data);
                 })
+        },
+        getAllTowns: function(success){
+        $http({method:'GET', url:'http://softuni-ads.azurewebsites.net/api/towns'})
+            .success(function(data, status, headers, config){
+                success(data);
+            })
+            .error(function(data, status, headers, config){
+                $log.warn(data);
+            })
         }
     }
 })
