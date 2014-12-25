05bac8cc6ff31bf8a9edafb50bbb70c764509a66
palazliev <palazliev@gmail.com>
2014-12-25 11:34:05 +0200

    added login/register links

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,14 @@
 var softUni = angular.module('softUniModule', ['ngRoute'])
-.config(function($routeProvider){
-       $routeProvider.when('/register',{
-           templateUrl:'templates/register.html',
-           controller:'SoftUniApp'
-       })
+    .config(function ($routeProvider) {
+        $routeProvider.when('/register', {
+            templateUrl: 'templates/register.html',
+            controller: 'SoftUniApp'
+        });
+        $routeProvider.when('/listAds', {
+            templateUrl: 'templates/listAds.html'
+        });
+        $routeProvider.when('/login', {
+            templateUrl: 'templates/login.html'
+        })
+
     });
