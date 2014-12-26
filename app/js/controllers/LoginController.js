3d550367e668a1fddddc70d8b4e86d0228eccf9e
palazliev <palazliev@gmail.com>
2014-12-26 17:03:25 +0200

    edited login

--- app/js/controllers/LoginController.js
+++ app/js/controllers/LoginController.js
@@ -1,5 +1,5 @@
 softUni.controller('loginController', function ($scope, loginService) {
-    $scope.currentUser={};
+
     $scope.login=function(user){
         loginService.login(user);
     }
--- app/js/services/loginService.js
+++ app/js/services/loginService.js
@@ -1,13 +1,26 @@
-softUni.factory('loginService', function($http){
-    return{
-        login:function(user){
-            var $promise=$http.post('http://softuni-ads.azurewebsites.net/api/user/login', user);
-            $promise.then(function(data) {
-                    console.log('success login');
-                }, function(msg){
-                    console.log('error login');
-                }
-            );
-        }
+softUni.factory('loginService', function($http, $q, $window){
+    var userInfo;
+
+    function login(user) {
+        var deferred = $q.defer();
+
+        $http.post('http://softuni-ads.azurewebsites.net/api/user/login', user)
+            .then(function(result) {
+            userInfo = {
+                accessToken: result.data.access_token,
+                userName: result.data.userName
+            };
+            $window.sessionStorage["userInfo"] = JSON.stringify(userInfo);
+            deferred.resolve(userInfo);
+        }, function(error) {
+            deferred.reject(error);
+        });
+
+        return deferred.promise;
     }
+
+    return {
+        login: login
+    };
+
 });
