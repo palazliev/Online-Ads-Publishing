1ecc337f372cc5eb312169890e1fcefeaec9dcdf
palazliev <palazliev@gmail.com>
2015-01-02 11:32:22 +0200

    fixed registration after register auto login and show home page

--- app/js/services/registrationService.js
+++ app/js/services/registrationService.js
@@ -6,14 +6,17 @@ softUni.factory('registrationService', function($http, $q, $window, $rootScope){
 
         $http.post('http://softuni-ads.azurewebsites.net/api/user/register', regUser)
             .then(function(result) {
-                $rootScope.regSccMsg="Registration successful. Please login.";
-                regUser.username="";
-                regUser.password="";
-                regUser.confirmPassword="";
-                regUser.email="";
-                regUser.name="";
-                regUser.phone="";
-                selected='';
+                $rootScope.userInfo = {
+                    accessToken: result.data.access_token,
+                    userName: result.data.username
+                };
+
+                $rootScope.isLoggedIn=true;
+                $window.sessionStorage["userInfo"] = JSON.stringify($rootScope.userInfo);
+                $window.sessionStorage["isLoggedIn"] = $rootScope.isLoggedIn;
+                deferred.resolve($rootScope.userInfo);
+                $window.location='#/';
+
             }, function(error) {
                 deferred.reject(error);
                 console.log("Registration error");
