a8742b9511301fd18ae1b31e393a23bca616b32b
palazliev <palazliev@gmail.com>
2015-01-03 09:49:49 +0200

    added redirect to home screen when attempt to access user page

--- app/js/app.js
+++ app/js/app.js
@@ -100,4 +100,15 @@ var softUni = angular.module('softUniModule', ['ngRoute'])
 
 
     });
+softUni.run(["$rootScope", "$location", function($rootScope, $location) {
+    $rootScope.$on("$routeChangeSuccess", function(userInfo) {
+        console.log(userInfo);
+    });
+
+    $rootScope.$on("$routeChangeError", function(event, current, previous, eventObj) {
+        if (eventObj.authenticated === false) {
+            $location.path("/");
+        }
+    });
+}]);
 
